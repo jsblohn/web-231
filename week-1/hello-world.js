780468b643d6ba3d57b5c1b5c0512eb83e32d7d1
Janet Blohn <jblohn@my365.bellevue.edu>
2019-11-29 17:44:49 -0600

    Added Code Attributions

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Janet Blohn 
+; Date: 29 November 2019
+; Modified By: N/A
+; Description: Exercise 1.3 - Hello World
+;===========================================
+*/
+
 console.log("Hello World, my name is Janet Blohn")
