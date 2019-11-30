7fe3cc5a9026fc2690afc1400a49b94ced83dba6
Janet Blohn <jblohn@my365.bellevue.edu>
2019-11-29 18:40:10 -0600

    Assignment 1.4

---
+++ week-1/blohn-assignment-1.4.js
@@ -0,0 +1,26 @@
+/*
+==============================================
+; Title: Assignment 1.1
+; Author: Janet Blohn   
+; Date: 29 November 2019
+; Modified By: N/A
+; Description: Assignment 1.4 - Primitive Types
+; actual age not given; I don't think it should
+; be required. It's not even legal to ask this
+; on a job application.
+;==============================================
+*/
+
+var firstName = "Janet";
+var lastName = "Blohn";
+var age = 99;
+var street = "12345 Fake Street";
+var city = "Omaha";
+var zipCode = 67890;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
